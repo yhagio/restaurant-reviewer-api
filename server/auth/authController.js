47e3ebfab83b59b5b77b86b374df94b46bacec22
Yuichi Hagio <yhagio87@gmail.com>
2016-07-09 11:04:27 -0400

    Signin

--- server/auth/authController.js
+++ server/auth/authController.js
@@ -6,7 +6,13 @@ exports.signin = function(req, res, next) {
   // verify user. Then we can just create a token
   // and send it back for the client to consume
   console.log('exports.signin ', req.user._id);
-  
+
   var token = signToken(req.user._id);
-  res.json({token: token});
+  res.json({
+    token: token,
+    user: {
+      username: req.user.username,
+      email: req.user.email
+    }
+  });
 };
