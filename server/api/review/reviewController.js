e1d01f833cbbeff107e4a671e3704e024fad42a7
Yuichi Hagio <yhagio87@gmail.com>
2016-07-13 21:35:58 -0400

    Create review, update restaurant async

--- server/api/review/reviewController.js
+++ server/api/review/reviewController.js
@@ -2,13 +2,13 @@ var Review = require('./reviewModel');
 var Restaurant = require('../restaurant/restaurantModel');
 
 exports.createReview = function(req, res, next) {
-  console.log('[createReview user]', req.user);
-  console.log('[createReview params]', req.params);
-  console.log('[createReview body]', req.body);
+  // console.log('[createReview user]', req.user);
+  // console.log('[createReview params]', req.params);
+  // console.log('[createReview body]', req.body);
 
   var comment = req.body.comment;
   var author = req.user._id;
-  var restaurant = req.params.id || 1;
+  var restaurant = req.params.id;
   var rating = req.body.rating;
 
 
@@ -19,12 +19,17 @@ exports.createReview = function(req, res, next) {
     rating: rating
   });
 
+  // Save review
   newReview.save(function(err, review) {
     if (err) return next(err);
-    
-    // Update the restaurant's total rating
+
+    // Increment the restaurant's total rating and add new one in reviews array
     var query = {_id: restaurant};
-    var update = {$inc: { total_ratings: rating}};
+    var update = {
+      $inc: { total_ratings: rating},
+      $addToSet: { reviews: review._id }
+    };
+
     Restaurant.findOneAndUpdate(query, update, function(error, result) {
       if (error) return next(error);
       return res.json(review);
--- server/api/review/reviewRoutes.js
+++ server/api/review/reviewRoutes.js
@@ -7,7 +7,7 @@ var checkUser = [auth.decodeToken(), auth.getFreshUser()];
 // Router.route('/restaurants/:id')
 //   .post(controller.createReview);
 
-Router.route('/create-review')
+Router.route('/:id/create-review')
   .post(checkUser, controller.createReview);
 
 module.exports = Router;
